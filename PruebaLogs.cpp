229d58f7b0b8591c7ae117c33b506808c97d334b
totto1230 <joseph.granados@hotmail.com>
2022-07-23 21:42:26 -0600

    DELETE AND UPDATE WORKING

--- PruebaLogs.cpp
+++ PruebaLogs.cpp
@@ -1,27 +1,40 @@
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+#include <ctime>
 
 using namespace std;
+ofstream file;
+
+char* time_testing() {
+	   // current date/time based on current system
+   time_t now = time(0);
+   
+   // convert now to string form
+   char* dt = ctime(&now);
+   return dt;
+}
 
 void logging(){
-	ofstream file;
+	//ofstream file;
 	file.open("TEST.txt",ios::out);
-	
+	char* test= time_testing();
 	if(file.fail()){
 		cout<<"ERROR WHILE LOGGING"<< endl;	
 		exit(1);
 	}
 //	string test= system("date";)
-	file<<""<< endl;
-	file<<"TEST"<< endl;
-	file<<"TEST"<< endl;
-	file.close();
+	//char a= time_testing();
+	file<<test<<""<< endl;
+	file<<test<<"TEST"<< endl;
+	file<<test<<"TEST"<< endl;
+	
 }
-
 
 int main (){
 	logging();
+	char* test= time_testing();
+	file<<test<<"ADDED FROM MAIN";
 	system("pause");
-	
+	file.close();
 }
